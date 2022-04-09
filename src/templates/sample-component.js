442abc18b70bd2e8ab5f91f70273d50b9c7cbdc6
Jairo Abraham Bernal <jabernal@gmail.com>
2022-04-09 11:01:33 -0500

    Updates

--- src/templates/sample-component.js
+++ src/templates/sample-component.js
@@ -1,13 +1,14 @@
-module.exports = `
-<template>
+module.exports = `<template>
   <div class="<%NAME_KEBAB%>" id="<%NAME_KEBAB%>">
-     
+     <h1>Title</h1>
   </div>
 </template>
 <script>
 export default  {
     name: "<%NAME%>",
     props: [],
+    emits: [], // Nueva manera de declara eventos personalizados den vue 3.
+    inject: [],
     mounted() {
       
     },
